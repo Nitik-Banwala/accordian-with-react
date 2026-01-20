49c95433b4983e49b645c99964b436428bc3a64b
NITIK <nitik@NITIKs-MacBook-Air.local>
2026-01-20 14:11:54 +0530

    initial commit

--- src/App.jsx
+++ src/App.jsx
@@ -3,7 +3,9 @@ import Accordion from './accordian'
 function App() {
   return (
     <>
-      <div className="min-h-screen flex items-center justify-center bg-gray-100">
+     
+      <div className="min-h-screen flex flex-col items-center justify-center bg-gray-100">
+        <h1 className='flex justify-center pb-10 text-4xl font-normal text-[#808080]'>Accordian</h1>
         <Accordion />
         
       </div>
--- src/accordian.jsx
+++ src/accordian.jsx
@@ -1,27 +1,26 @@
 import { useState } from "react";
 import { ArrowDown } from "./icons";
 
-
 export default function Accordion() {
     const data = [
         {
-            title: "Accordion 1",
-            content: "Lorem ipsum dolor sit amet Lorem  ipsum dolor sit amet..",
-        },
-        {
-            title: "Accordion 2",
+            title: "Group-1",
             content: "Lorem ipsum dolor sit amet Lorem ipsum dolor sit amet..",
         },
         {
-            title: "Accordion 3",
+            title: "Group-2",
             content: "Lorem ipsum dolor sit amet Lorem ipsum dolor sit amet..",
         },
         {
-            title: "Accordion 4",
+            title: "Group-3",
             content: "Lorem ipsum dolor sit amet Lorem ipsum dolor sit amet..",
         },
         {
-            title: "Accordion 5",
+            title: "Group-4",
+            content: "Lorem ipsum dolor sit amet Lorem ipsum dolor sit amet..",
+        },
+        {
+            title: "Group-5",
             content: "Lorem ipsum dolor sit amet Lorem ipsum dolor sit amet..",
         },
     ];
@@ -33,16 +32,25 @@ export default function Accordion() {
     };
 
     return (
-        <div className="max-w-100  space-y-3">
+        <div className="p-4 bg-white shadow-sm rounded-2xl">
+        <div className="max-w-100 space-y-3">
             {data.map((item, index) => (
-                <div key={index} className="border rounded-lg overflow-hidden">
-
+                <div
+                    key={index}
+                    className={`rounded-lg overflow-hidden border transition-colors duration-300 ${openIndex === index
+                            ? "border-blue-500"
+                            : "border-gray-300"
+                        }`}
+                >
+                    
                     <button
                         onClick={() => toggle(index)}
-                        className="w-full flex justify-between items-center p-4 font-semibold"
+                        className={`w-full flex justify-between items-center p-4 font-semibold transition-colors ${openIndex === index
+                                ? "text-gray-800 bg-white"
+                                : "text-gray-800 bg-white"
+                            }`}
                     >
-                        {item.title}
-
+                        <h3 className="text-[#808080] font-normal">{item.title}</h3>
                         <span
                             className={`transition-transform duration-300 ${openIndex === index ? "rotate-180" : ""
                                 }`}
@@ -52,13 +60,17 @@ export default function Accordion() {
                     </button>
 
                     <div
-                        className={`overflow-hidden transition-all duration-300 ${openIndex === index ? "max-h-40 p-4" : "max-h-0 p-0"
+                        className={`overflow-hidden transition-all duration-300 ${openIndex === index
+                                ? "max-h-40 p-4"
+                                : "max-h-0 p-0"
                             }`}
                     >
-                        <p className="text-gray-600">{item.content}</p>
+                        <p className="text-gray-400">{item.content}</p>
+                        
                     </div>
                 </div>
             ))}
+            </div>
         </div>
     );
 }
